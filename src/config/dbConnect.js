cdf5388165a39e02cb784e3affcebfe33710e85f
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2022-04-24 01:16:15 -0300

    Delete dbConnect.js

--- src/config/dbConnect.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-
-mongoose.connect(
-  'mongodb+srv://wendreslucas:NewHorizons7@node-project.nodes.mongodb.net/nodelivros'
-)
-
-let db = mongoose.connection
-
-export default db
